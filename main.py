970b893d5e24d91269b52d423408b2d28fd6a14b
toomuchlychee <66142908+toomuchlychee@users.noreply.github.com>
2020-06-26 14:23:27 -0500

    IT WORKS??? sometimes
    if the final word is anywhere in the output message, code returns an error otherwise it works

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 import random
-from itertools import count
 
 
 """class to contain a word and its possible followers in a list
@@ -11,7 +10,7 @@ class Word:
 
     def __init__(self, instance, **kwargs):             # initialize the word
         self.instance = instance                        # instance is the referenced word
-        self.trees = []                                 # trees are the possible words that can come after it
+        self.trees = []                                 # trees are the possible words_raw that can come after it
         if 'follower' in kwargs:                        # adds the following word to trees
             self.trees.append(kwargs['follower'])
         if 'punctuation' in kwargs:
@@ -25,7 +24,6 @@ class WordContainer(dict):
     @classmethod
     def create(cls, e_list):
         returned_dict = cls()
-
         for w, n in zip(e_list, e_list[1:]):
             if w not in returned_dict.added_ws():
                 returned_dict.update({w: Word(w, follower=n)})
@@ -45,26 +43,37 @@ def find_whitespace(text):
 
 """main"""
 
-#  set up objects Words in Word_Container
+#  set up objects in Word_Container
 
 read_doc = open('document.txt', 'r')
 long_string = read_doc.read()
 spaces = find_whitespace(long_string)
 # print(spaces)
-WORDS = [first_word := long_string[:spaces[0]]]
-# creating the WORDS list of strings
+words_raw = [first_word := long_string[:spaces[0]]]
+# creating the words_raw list of strings
 for n in range(1, len(spaces)):
-    WORDS.append(long_string[spaces[n-1]:spaces[n]])
-WORDS.append(last_word := long_string[spaces[-1]:])
-for w in WORDS:
-    w = w.lstrip()  # removes whitespace
-word_container = WordContainer.create(WORDS)
+    words_raw.append(long_string[spaces[n-1]:spaces[n]])
+words_raw.append(last_word := long_string[spaces[-1]:])
+words_stripped = []
+for w in words_raw:
+    words_stripped.append(w.lstrip())
+word_container = WordContainer.create(words_stripped)
 # print(word_container)
 
 #  create the list of string outputs
 
 output_list = []
-print(word_container[WORDS[0]].trees)
+# add the 1st word of words_stripped to output_list
+output_list.append(words_stripped[0])
+for n in range(len(words_stripped)-1):
+    if word_container[output_list[n]].trees:
+        output_list.append(random.choice(word_container[output_list[n]].trees))
+# print(output_list)
+output_string = ''
+for s in output_list:
+    output_string += ' '
+    output_string += s
+print(output_string)
 
 
 
